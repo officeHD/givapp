204d2c45848c68962e92c25c91c4377344354670
officeHD <haungdongoffice@gmail.com>
2019-07-25 09:36:56 +0800

    详情页

--- src/native/app/goods/goods.vue
+++ src/native/app/goods/goods.vue
@@ -1,89 +1,11 @@
 
- <style  scoped>
-.swiper-box{
-	position: relative;
-	width: 750px;
-	height: 750px;
-}
-.slider{
-width: 750px;
-height: 750px
-}
-.frame {
-width: 100%;
-height: 750px;
-}
- .itemImg{
-	width: 750px;
-	height: 750px;
- }
-
- 
-.info-box {
-	width:  710px;
-	padding: 20px;
-	background-color: #fff;
-	margin-bottom: 20px;
-}
-.goods-info-price{
-	font-size: 46px;
-		font-weight: 600;
-		color: #f47925;
-}
-.goods-info-title{
-		font-size: 30px;
-}
-.spec-row{
-	width:670px;
-		align-items: center;
-		margin: 0 0 30px 0;
-	 flex-direction:row;
-}
-.spec-text{
-	font-size: 24px;
-			color: #a2a2a2;
-			margin-right: 20px;
-}
-.spec-content{
-		font-size: 28px;
-			flex-wrap: wrap;
-			 flex-direction:row;
-			  
-}
-.spec-serviceitem{
-	margin-right: 10px;
-}
-.spec-sp{
-	flex:1;
- flex-direction:row;
-flex-wrap: wrap;
-}
-.spec-sp-item{
-background-color: #f6f6f6;
-					padding: 5px 15px;
-					color: #999;
-					margin-right: 10px;
-					font-size: 20px;
-					border-radius: 5px;
-}
-.spec-sp-item-on{
-	border: solid 1px #f47952;
-						padding: 4px 8px;
-}
-.sp-title{
-	 width:120px;
-	 padding:5px 0;
-}
-</style>
-
 <template>
-	<div>
-		
+	<div> 
 		<!-- 商品主图轮播 -->
 		<div class="swiper-box">
 		 <slider class="slider" interval="3000" auto-play="true">
 			<div class="frame"  v-for="swiper in swiperList" :key="swiper.id">
-				<img class="itemImg" :src="swiper.img"></img>
+				<image class="itemImg" :src="swiper.img"></image>
 			</div>
 			<indicator></indicator>
 		</slider>
@@ -128,7 +50,8 @@ background-color: #f6f6f6;
 			</div>
 			<div class="comment" @tap="toRatings">
 				<div class="user-info">
-					<div class="face"><img :src="goodsData.comment.userface"></img></div>
+					<div class="face">
+						<image class="faceimg" :src="goodsData.comment.userface"></image></div>
 					<div class="username">{{goodsData.comment.username}}</div>
 				</div>
 				<div class="content">
@@ -193,28 +116,16 @@ background-color: #f6f6f6;
 				<text class="h1">分享</text>
 				<div class="list">
 					<div class="box">
-						<img src="../../static/img/share/wx.png"></img>
-						<text class="title">
-							微信好友
-						</text>
+						<text class="title"> 微信好友 </text>
 					</div>
 					<div class="box">
-						<img src="../../static/img/share/pyq.png"></img>
-						<text class="title">
-							朋友圈
-						</text>
+						<text class="title"> 朋友圈 </text>
 					</div>
 					<div class="box">
-						<img src="../../static/img/share/wb.png"></img>
-						<text class="title">
-							新浪微博
-						</text>
+						<text class="title"> 新浪微博 </text>
 					</div>
 					<div class="box">
-						<img src="../../static/img/share/qq.png"></img>
-						<text class="title">
-							QQ
-						</text>
+						<text class="title"> QQ </text>
 					</div>
 				</div>
 				<text class="btn" @tap="hideShare">
@@ -270,210 +181,314 @@ background-color: #f6f6f6;
 
 <script>
 export default {
-	data() {
-		return {
-		 
-			//是否显示返回按钮
-			// #ifndef MP
-			showBack:true,
-			// #endif
-			//轮播主图数据
-			swiperList: [
-				{ id: 1, img: 'https://ae01.alicdn.com/kf/HTB1Mj7iTmzqK1RjSZFjq6zlCFXaP.jpg' },
-				{ id: 2, img: 'https://ae01.alicdn.com/kf/HTB1fbseTmzqK1RjSZFLq6An2XXaL.jpg' },
-				{ id: 3, img: 'https://ae01.alicdn.com/kf/HTB1dPUMThnaK1RjSZFtq6zC2VXa0.jpg' },
-				{ id: 4, img: 'https://ae01.alicdn.com/kf/HTB1OHZrTXzqK1RjSZFvq6AB7VXaw.jpg' }
-			],
-			//轮播图下标
-			currentSwiper: 0,
-			anchorlist:[],//导航条锚点
-			selectAnchor:0,//选中锚点
-			serviceClass: '',//服务弹窗css类，控制开关动画
-			specClass: '',//规格弹窗css类，控制开关动画
-			shareClass:'',//分享弹窗css类，控制开关动画
-			// 商品信息
-			goodsData:{
-				id:1,
-				name:"商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题",
-				price:"127.00",
-				number:1,
-				service:[
-					{name:"正品保证",description:"此商品官方保证为正品"},
-					{name:"极速退款",description:"此商品享受退货极速退款服务"},
-					{name:"7天退换",description:"此商品享受7天无理由退换服务"}
-				],
-				spec:["XS","S","M","L","XL","XXL"],
-				comment:{
-					number:102,
-					userface:'../../static/img/face.jpg',
-					username:'大黑哥',
-					content:'很不错，之前买了很多次了，很好看，能放很久，和图片色差不大，值得购买！'
-				}
-			},
-			selectSpec:null,//选中规格
-			isKeep:false,//收藏
-			//商品描述html
-			descriptionStr:'<div style="text-align:center;"><img width="100%" src="https://ae01.alicdn.com/kf/HTB1t0fUl_Zmx1VjSZFGq6yx2XXa5.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB1LzkjThTpK1RjSZFKq6y2wXXaT.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB18dkiTbvpK1RjSZPiq6zmwXXa8.jpg"/></div>'
-		};
-	},
- 
-	mounted () {
-		
-	},
-	methods: {
-		//轮播图指示器
-		swiperChange(event) {
-			this.currentSwiper = event.detail.current;
-		},
-		//消息列表
-		toMsg(){
-			uni.navigateTo({
-				url:'../msg/msg'
-			})
-		},
-		// 客服
-		toChat(){
-			uni.navigateTo({
-				url:"../msg/chat/chat?name=客服008"
-			})
-		},
-		// 分享
-		share(){
-			this.shareClass = 'show';
-		},
-		hideShare(){
-			this.shareClass = 'hide';
-			setTimeout(() => {
-				this.shareClass = 'none';
-			}, 150);
-		},
-		//收藏
-		keep(){
-			this.isKeep = this.isKeep?false:true;
-		},
-		// 加入购物车
-		joinCart(){
-			if(this.selectSpec==null){
-				return this.showSpec(()=>{
-					uni.showToast({title: "已加入购物车"});
-				});
-			}
-			uni.showToast({title: "已加入购物车"});
-		},
-		//立即购买
-		buy(){
-			if(this.selectSpec==null){
-				return this.showSpec(()=>{
-					this.toConfirmation();
-				});
-			}
-			this.toConfirmation();
-		},
-		//商品评论
-		toRatings(){
-			uni.navigateTo({
-				url:'ratings/ratings'
-			})
-		},
-		//跳转确认订单页面
-		toConfirmation(){
-			let tmpList=[];
-			let goods = {id:this.goodsData.id,img:'../../static/img/goods/p1.jpg',name:this.goodsData.name,spec:'规格:'+this.goodsData.spec[this.selectSpec],price:this.goodsData.price,number:this.goodsData.number};
-			tmpList.push(goods);
-			uni.setStorage({
-				key:'buylist',
-				data:tmpList,
-				success: () => {
-					uni.navigateTo({
-						url:'../order/confirmation'
-					})
-				}
-			})
-		},
-		//跳转评论列表
-		showComments(goodsid){
-			
-		},
-		//选择规格
-		setSelectSpec(index){
-			this.selectSpec = index;
-		},
-		//减少数量
-		sub(){
-			if(this.goodsData.number<=1){
-				return;
-			}
-			this.goodsData.number--;
-		},
-		//增加数量
-		add(){
-			this.goodsData.number++;
-		},
-		//跳转锚点
-		toAnchor(index){
-			this.selectAnchor = index;
-			uni.pageScrollTo({scrollTop: this.anchorlist[index].top,duration: 200});
-		},
-		//计算锚点高度
-		calcAnchor(){
-			this.anchorlist=[
-				{name:'主图',top:0},
-				{name:'评价',top:0},
-				{name:'详情',top:0}
-			]
-			let commentsdiv = uni.createSelectorQuery().select("#comments");
-			commentsdiv.boundingClientRect((data) => {
-				let statusbarHeight = 0;
-				//APP内还要计算状态栏高度
-				// #ifdef APP-PLUS
-					statusbarHeight = plus.navigator.getStatusbarHeight()
-				// #endif
-				let headerHeight = uni.px2px(100);
-				this.anchorlist[1].top = data.top - headerHeight - statusbarHeight;
-				this.anchorlist[2].top = data.bottom - headerHeight - statusbarHeight;
-				
-			}).exec();
-		},
-		//返回上一页
-		back() {
-			uni.navigateBack();
-		},
-		//服务弹窗
-		showService() {
-			console.log('show');
-			this.serviceClass = 'show';
-		},
-		//关闭服务弹窗
-		hideService() {
-			this.serviceClass = 'hide';
-			setTimeout(() => {
-				this.serviceClass = 'none';
-			}, 200);
-		},
-		//规格弹窗
-		showSpec(fun) {
-			console.log('show');
-			this.specClass = 'show';
-			this.specCallback = fun;
-		},
-		specCallback(){
-			return;
-		},
-		//关闭规格弹窗
-		hideSpec() {
-			this.specClass = 'hide';
-			//回调
+  data() {
+    return {
+      //是否显示返回按钮
+      // #ifndef MP
+      showBack: true,
+      // #endif
+      //轮播主图数据
+      swiperList: [
+        {
+          id: 1,
+          img: "https://ae01.alicdn.com/kf/HTB1Mj7iTmzqK1RjSZFjq6zlCFXaP.jpg"
+        },
+        {
+          id: 2,
+          img: "https://ae01.alicdn.com/kf/HTB1fbseTmzqK1RjSZFLq6An2XXaL.jpg"
+        },
+        {
+          id: 3,
+          img: "https://ae01.alicdn.com/kf/HTB1dPUMThnaK1RjSZFtq6zC2VXa0.jpg"
+        },
+        {
+          id: 4,
+          img: "https://ae01.alicdn.com/kf/HTB1OHZrTXzqK1RjSZFvq6AB7VXaw.jpg"
+        }
+      ],
+      //轮播图下标
+      currentSwiper: 0,
+      anchorlist: [], //导航条锚点
+      selectAnchor: 0, //选中锚点
+      serviceClass: "", //服务弹窗css类，控制开关动画
+      specClass: "", //规格弹窗css类，控制开关动画
+      shareClass: "", //分享弹窗css类，控制开关动画
+      // 商品信息
+      goodsData: {
+        id: 1,
+        name:
+          "商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题商品标题",
+        price: "127.00",
+        number: 1,
+        service: [
+          { name: "正品保证", description: "此商品官方保证为正品" },
+          { name: "极速退款", description: "此商品享受退货极速退款服务" },
+          { name: "7天退换", description: "此商品享受7天无理由退换服务" }
+        ],
+        spec: ["XS", "S", "M", "L", "XL", "XXL"],
+        comment: {
+          number: 102,
+          userface: "../../static/img/face.jpg",
+          username: "大黑哥",
+          content:
+            "很不错，之前买了很多次了，很好看，能放很久，和图片色差不大，值得购买！"
+        }
+      },
+      selectSpec: null, //选中规格
+      isKeep: false, //收藏
+      //商品描述html
+      descriptionStr:
+        '<div style="text-align:center;"><img width="100%" src="https://ae01.alicdn.com/kf/HTB1t0fUl_Zmx1VjSZFGq6yx2XXa5.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB1LzkjThTpK1RjSZFKq6y2wXXaT.jpg"/><img width="100%" src="https://ae01.alicdn.com/kf/HTB18dkiTbvpK1RjSZPiq6zmwXXa8.jpg"/></div>'
+    };
+  },
 
-			this.selectSpec&&this.specCallback&&this.specCallback();
-			this.specCallback = false;
-			setTimeout(() => {
-				this.specClass = 'none';
-			}, 200);
-		},
-		discard() {
-			//丢弃
-		}
-	}
+  mounted() {},
+  methods: {
+    //轮播图指示器
+    swiperChange(event) {
+      this.currentSwiper = event.detail.current;
+    },
+    //消息列表
+    toMsg() {
+      uni.navigateTo({
+        url: "../msg/msg"
+      });
+    },
+    // 客服
+    toChat() {
+      uni.navigateTo({
+        url: "../msg/chat/chat?name=客服008"
+      });
+    },
+    // 分享
+    share() {
+      this.shareClass = "show";
+    },
+    hideShare() {
+      this.shareClass = "hide";
+      setTimeout(() => {
+        this.shareClass = "none";
+      }, 150);
+    },
+    //收藏
+    keep() {
+      this.isKeep = this.isKeep ? false : true;
+    },
+    // 加入购物车
+    joinCart() {
+      if (this.selectSpec == null) {
+        return this.showSpec(() => {
+          uni.showToast({ title: "已加入购物车" });
+        });
+      }
+      uni.showToast({ title: "已加入购物车" });
+    },
+    //立即购买
+    buy() {
+      if (this.selectSpec == null) {
+        return this.showSpec(() => {
+          this.toConfirmation();
+        });
+      }
+      this.toConfirmation();
+    },
+    //商品评论
+    toRatings() {
+      uni.navigateTo({
+        url: "ratings/ratings"
+      });
+    },
+    //跳转确认订单页面
+    toConfirmation() {
+      let tmpList = [];
+      let goods = {
+        id: this.goodsData.id,
+        img: "../../static/img/goods/p1.jpg",
+        name: this.goodsData.name,
+        spec: "规格:" + this.goodsData.spec[this.selectSpec],
+        price: this.goodsData.price,
+        number: this.goodsData.number
+      };
+      tmpList.push(goods);
+      uni.setStorage({
+        key: "buylist",
+        data: tmpList,
+        success: () => {
+          uni.navigateTo({
+            url: "../order/confirmation"
+          });
+        }
+      });
+    },
+    //跳转评论列表
+    showComments(goodsid) {},
+    //选择规格
+    setSelectSpec(index) {
+      this.selectSpec = index;
+    },
+    //减少数量
+    sub() {
+      if (this.goodsData.number <= 1) {
+        return;
+      }
+      this.goodsData.number--;
+    },
+    //增加数量
+    add() {
+      this.goodsData.number++;
+    },
+    //跳转锚点
+    toAnchor(index) {
+      this.selectAnchor = index;
+      uni.pageScrollTo({
+        scrollTop: this.anchorlist[index].top,
+        duration: 200
+      });
+    },
+    //计算锚点高度
+    calcAnchor() {
+      this.anchorlist = [
+        { name: "主图", top: 0 },
+        { name: "评价", top: 0 },
+        { name: "详情", top: 0 }
+      ];
+      let commentsdiv = uni.createSelectorQuery().select("#comments");
+      commentsdiv
+        .boundingClientRect(data => {
+          let statusbarHeight = 0;
+          //APP内还要计算状态栏高度
+          // #ifdef APP-PLUS
+          statusbarHeight = plus.navigator.getStatusbarHeight();
+          // #endif
+          let headerHeight = uni.px2px(100);
+          this.anchorlist[1].top = data.top - headerHeight - statusbarHeight;
+          this.anchorlist[2].top = data.bottom - headerHeight - statusbarHeight;
+        })
+        .exec();
+    },
+    //返回上一页
+    back() {
+      uni.navigateBack();
+    },
+    //服务弹窗
+    showService() {
+      console.log("show");
+      this.serviceClass = "show";
+    },
+    //关闭服务弹窗
+    hideService() {
+      this.serviceClass = "hide";
+      setTimeout(() => {
+        this.serviceClass = "none";
+      }, 200);
+    },
+    //规格弹窗
+    showSpec(fun) {
+      console.log("show");
+      this.specClass = "show";
+      this.specCallback = fun;
+    },
+    specCallback() {
+      return;
+    },
+    //关闭规格弹窗
+    hideSpec() {
+      this.specClass = "hide";
+      //回调
+
+      this.selectSpec && this.specCallback && this.specCallback();
+      this.specCallback = false;
+      setTimeout(() => {
+        this.specClass = "none";
+      }, 200);
+    },
+    discard() {
+      //丢弃
+    }
+  }
 };
 </script>
+
+ <style  scoped>
+.swiper-box {
+  position: relative;
+  width: 750px;
+  height: 750px;
+}
+.slider {
+  width: 750px;
+  height: 750px;
+}
+.frame {
+  width: 100%;
+  height: 750px;
+}
+.itemImg {
+  width: 750px;
+  height: 750px;
+}
+
+.info-box {
+  width: 710px;
+  padding: 20px;
+  background-color: #fff;
+  margin-bottom: 20px;
+}
+.goods-info-price {
+  font-size: 46px;
+  font-weight: 600;
+  color: #f47925;
+}
+.goods-info-title {
+  font-size: 30px;
+}
+.spec-row {
+  width: 670px;
+  align-items: center;
+  margin: 0 0 30px 0;
+  flex-direction: row;
+}
+.spec-text {
+  font-size: 24px;
+  color: #a2a2a2;
+  margin-right: 20px;
+}
+.spec-content {
+  font-size: 28px;
+  flex-wrap: wrap;
+  flex-direction: row;
+}
+.spec-serviceitem {
+  margin-right: 10px;
+}
+.spec-sp {
+  flex: 1;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+.spec-sp-item {
+  background-color: #f6f6f6;
+  padding: 5px 15px;
+  color: #999;
+  margin-right: 10px;
+  font-size: 20px;
+  border-radius: 5px;
+}
+.spec-sp-item-on {
+  border: solid 1px #f47952;
+  padding: 4px 8px;
+}
+.sp-title {
+  width: 120px;
+  padding: 5px 0;
+}
+.boxIcon{
+	width:60px;
+	height:60px;
+}
+.faceimg{
+	width:60px;
+	height:60px;
+}
+</style>
  
