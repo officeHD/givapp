f18fbedb68d7986dceeed59495ff2157227efdff
officeHd <haungdongoffice@gmail.com>
2019-08-09 07:28:42 +0800

    Signed-off-by: officeHd <haungdongoffice@gmail.com>

--- src/native/app/selling/selling.vue
+++ src/native/app/selling/selling.vue
@@ -21,8 +21,63 @@
 					<text class="newType">Heifei High-tech Zone,Anhui Province</text>
 				</div>
 			</div>
-			<div class="selBox mt20"></div>
+			<div class=" mt20">
+				<div class="locationBox bb " @click="changeAlert('show')">
+					<text class="meth_title">Price</text>
+					<div class="meth_right ">
+						<text class="cityName">Woman</text>
+						<text class="rightIcon">&#xe6a1;</text>
+					</div>
+				</div>
+				<div class="locationBox bb " @click="changeAlert('show')">
+					<text class="meth_title">Price</text>
+					<div class="meth_right ">
+						<text class="cityName">Categories </text>
+						<text class="rightIcon">&#xe6a1;</text>
+					</div>
+				</div>
+				<div class="locationBox bb " @click="changeAlert('show')">
+					<text class="meth_title">Trading method</text>
+					<div class="meth_right ">
+						<text class="cityName">Woman</text>
+						<text class="rightIcon">&#xe6a1;</text>
+					</div>
+				</div>
+			</div>
 		</scroller>
+		<div :class="[showMask?'maskAlert':'hidemask']" @click="changeAlert('hide')"></div>
+
+		<!-- 性别弹出框 -->
+		<div class="shopAlert" ref="shopAlert" @click="e=>e.stopPropagation()">
+			<!-- <text class="alertTop">Shipping method</text> -->
+			<div class="alertScroll">
+				<div class="selectLi">
+					<text class="liTit">Price</text>
+					<input class="liInput" type="number" placeholder="">
+				</div>
+				<div class="selectLi">
+					<text class="liTit">Original price</text>
+				</div>
+				<div class="selectLi">
+					<text class="liTit">GIV delivery</text>
+					<div class="liCon">
+						<div class="radioL">
+							<text class="liIcon" :class="[user.sex=='0'?'selected':'']">{{user.sex=="0"?"&#xe602;":"&#xe67f;"}}</text>
+							<text class="liTxt" :class="[user.sex=='0'?'selected':'']">Free shipping</text>
+					
+						</div>
+						<div class="radioL">
+							<text class="liIcon" :class="[user.sex=='1'?'selected':'']">{{user.sex=="1"?"&#xe602;":"&#xe67f;"}}</text>
+							<text class="liTxt" :class="[user.sex=='1'?'selected':'']">distance</text>
+					
+						</div>
+					</div>
+				</div>
+
+
+			</div>
+			<text class="sureBtn" @click="confirm">confirm</text>
+		</div>
 	</div>
 </template>
 
@@ -30,12 +85,16 @@
 	const navigator = weex.requireModule("navigator");
 	const photo = weex.requireModule("photo");
 	const net = weex.requireModule("net");
-
+	const animation = weex.requireModule("animation");
 	export default {
 		data() {
 			return {
+				showMask: false,
 				amount: 0,
-				src: ""
+				src: "",
+				user: {
+					sex: 1
+				}
 			};
 		},
 		onLoad(e) {
@@ -44,6 +103,27 @@
 		methods: {
 			returnpage() {
 				navigator.backTo("A");
+			},
+			changeAlert(type) {
+				var testEl = this.$refs.shopAlert;
+				let transform = "100%";
+				this.showMask = false;
+
+				if (type == "show") {
+					transform = "-100%";
+					this.showMask = true;
+				}
+				animation.transition(
+					testEl, {
+						styles: {
+							transform: `translateY(${transform})`
+						},
+						duration: 400, //ms
+						timingFunction: "ease",
+						delay: 0 //ms
+					},
+					function() {}
+				);
 			},
 			pickPhoto() {
 				var self = this;
@@ -80,7 +160,7 @@
 						}
 					);
 				});
-			
+
 			}
 		}
 	};
@@ -157,5 +237,153 @@
 		margin-left: 30px;
 		font-size: 34px;
 		color: #999999;
+	}
+
+	.bb {
+		border-bottom-style: solid;
+		border-bottom-color: #EDEDED;
+		border-bottom-width: 1px;
+	}
+
+	.locationBox {
+		width: 750px;
+
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+		height: 104px;
+		padding: 0 32px;
+		/* margin-bottom: 20px; */
+		background-color: #FFFFFF;
+	}
+
+	.meth_title {
+		color: #323232;
+		font-size: 30px;
+		width: 230px;
+	}
+
+	.meth_right {
+		flex: 1;
+		flex-direction: row;
+		justify-content: flex-end;
+		align-items: center;
+
+	}
+
+	.cityName {
+
+		margin-right: 30px;
+		font-size: 28px;
+		color: #999999;
+		flex: 1;
+		text-align: right;
+	}
+
+	.rightIcon {
+		font-family: iconfont;
+		font-size: 26px;
+		color: #999;
+	}
+
+	.hidemask {
+		height: 0;
+	}
+
+	.maskAlert {
+		position: fixed;
+		left: 0;
+		right: 0;
+		bottom: 500px;
+		top: 0;
+		/* background-color: rgba(0, 0, 0, 0); */
+	}
+
+	.shopAlert {
+		position: fixed;
+		left: 0;
+		bottom: -500px;
+		width: 750px;
+		height: 500px;
+		background-color: #ffffff;
+		border-top-left-radius: 20px;
+		border-top-right-radius: 20px;
+		align-items: center;
+		padding: 32px;
+	}
+
+	.alertTop {
+		width: 686px;
+		text-align: left;
+		padding-bottom: 32px;
+		font-size: 28px;
+		color: #333333;
+	}
+
+	.alertScroll {
+		flex: 1;
+		width: 686px;
+
+	}
+
+	.selectLi {
+		/* margin-bottom: 30px; */
+		width: 686px;
+		height: 100px;
+		flex-direction: row;
+		align-items: center;
+		justify-content: space-between;
+
+	}
+
+	.liTit {
+		width: 180px;
+		font-size: 28px;
+		color: #333333;
+	}
+	.liInput{
+		flex: 1;
+		text-align: right;
+		background-color: #0088FB;
+		height: 80px;
+	}
+
+	.liCon {
+		flex: 1;
+		flex-direction: row;
+		align-items: center;
+	}
+
+	.radioL {
+		flex-direction: row;
+		margin-right: 20px;
+	}
+
+	.liIcon {
+		font-size: 40px;
+		font-family: iconfont;
+		color: #999999;
+		margin-right: 10px;
+	}
+
+	.liTxt {
+		color: #999999;
+		font-size: 28px;
+	}
+
+	.selected {
+		color: #BA8833;
+	}
+
+	.sureBtn {
+		width: 686px;
+		height: 80px;
+		line-height: 80px;
+		border-radius: 10px;
+		margin-bottom: 14px;
+		background-color: #303030;
+		color: #fff;
+		text-align: center;
+		font-size: 30px;
 	}
 </style>
